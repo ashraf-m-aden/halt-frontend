942da5daf7209077271c4502c97f39ea81baeb86
nejishow <ashrafmuhamad2014@gmail.com>
2021-11-28 23:53:53 +0300

    first commit

---
+++ components/AboutTwo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="section-padding">
+        <div class="container">
+            <div class="row">
+                <!-- About Coontent Start -->
+                <div class="col-lg-6" data-aos="fade-up">
+                    <div class="about-content-area">
+                        <!-- Section Title Two Start -->
+                        <div class="section-title-two">
+                            <span class="sub-title">Nos réalisations</span>
+                            <h3 class="title">Mettre ici une phrase <br> d'accroche</h3>
+                        </div>
+                        <!-- Section Title Two End -->
+
+                        <div class="row mtn-30">
+                            <div class="col-md-5 col-sm-6 col-12 mt-30" data-aos="fade-up">
+                                <div class="about-funfact">
+                                    <div class="number"><span class="counter">110</span>+</div>
+                                    <h6 class="text">Clients satisfaits</h6>
+                                    <p>Nous aidons nos clients à augmenter leurs profits en leur fournissons un service de qualité.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-5 col-sm-6 col-12 mt-30" data-aos="fade-up">
+                                <div class="about-funfact">
+                                    <div class="number"><span class="counter">130</span>+</div>
+                                    <h6 class="text">Projets completés</h6>
+                                    <p>Nous aidons nos clients à augmenter leurs profits en leur fournissons un service de qualité.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- About Coontent Start -->
+
+                <!-- About Image Area Start -->
+                <div class="col-lg-6" data-aos="fade-up">
+                    <div class="about-image-area about-two about-shape-animation right-0 mr-0">
+                        <div class="about-image js-tilt">
+                            <img src="../static/images/about/about-1.jpg" alt="image">
+                        </div>
+                        <div class="about-image js-tilt">
+                            <img src="/images/about/about-2.jpg" alt="image">
+                        </div>
+                        <!-- Animation Shape Start -->
+                        <ShapeWithAnimation addClassName="shape-1" data-depth="1" imgSrc="/images/shape-animation/video-shape-1.png" />
+                    </div>
+                </div>
+                <!-- About Image Area Start -->
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/HeroFixedBanner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="intro-slider">
+        <div class="intro-section overlay bg-cover" :style="{ backgroundImage: `url('/images/hero-image/corporate.jfif')` }">
+            <div class="container">
+                <div class="row row-cols-lg-1 row-cols-1">
+                    <div class="col align-self-center">
+                        <div class="intro-content m-auto text-center">
+                            <h2 class="title">Réaliser vos projets</h2>
+                                <client-only>
+                                    <vue-typer  
+                                        :text='["correctement","avec succés","et professionalisme"]'
+                                        erase-style='backspace'
+                                        :erase-delay='100'
+                                    ></vue-typer>
+                                </client-only>
+                            <div class="desc">
+                                <p>Nous sommes une agence située à Djibouti. Nous pensons stratégie, concevons, et réalisons vos objectis.</p>
+                            </div>
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
